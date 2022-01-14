d354cbabcb6835dd5e7ae51a16dae4df75cff0f5
eishin.shu <cc93516@gmail.com>
2022-01-14 15:33:32 +0900

    getAllMessagesを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,17 @@
-package controllers;
+package controllers; 		//データベースから複数のメッセージ情報を取得して
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -27,8 +32,16 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+       EntityManager em = DBUtil.createEntityManager();
+
+       //Message.javaでつけた名前 getAllMessages を ↓createNamedQuery メソッドの引数に指定してあげることで、データベースへの問い合わせを実行
+       //その問い合わせ結果を getResultList() メソッドを使ってリスト形式で取得
+       //データベースに保存されたデータはHibernateによって自動で Message クラスのオブジェクトになってこのリストの中に格納される
+       List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+       //データの登録件数を表示
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
     }
 
 }
